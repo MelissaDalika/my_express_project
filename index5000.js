48ad06703a34035285aaf381c7496b58b17476db
Melissa Lopez Valdez <melissa.melisita@gmail.com>
2020-07-03 08:11:11 +0000

    change path

--- index5000.js
+++ index5000.js
@@ -3,13 +3,13 @@ const path = require('path')
 const app = express()
 
 app.get('/utenti', (req, res) => {
-  res.status(200).sendFile(path.join(__dirname,  '/utenti.html'))
+  res.status(200).sendFile(path.join(__dirname,  '/views/utenti.html'))
 })
 app.get('/articoli', (req, res) => {
-  res.status(200).sendFile(path.join(__dirname,  '/articoli.html'))
+  res.status(200).sendFile(path.join(__dirname,  '/views/articoli.html'))
 })
 app.get('*', (req, res) => {
-  res.status(200).sendFile(path.join(__dirname,  '/notfound.html'))
+  res.status(200).sendFile(path.join(__dirname,  '/views/notfound.html'))
 })
 
-app.listen( 5000, () => console.log('Listening on port 5000'));
+app.listen( 5000, () => console.log('5000'));
