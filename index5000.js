323a5671c2e420d7340fb109eafdef650ad7b185
Melissa Lopez Valdez <melissa.melisita@gmail.com>
2020-07-03 10:35:18 +0000

    req params and req query response

--- index5000.js
+++ index5000.js
@@ -2,14 +2,29 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-app.get('/utenti', (req, res) => {
-  res.status(200).sendFile(path.join(__dirname,  '/views/utenti.html'))
+
+app.use('/', express.static("public"));
+
+app.get('/about', (req, res) => {
+  res.status(200).sendFile(path.join(__dirname,  '/views/about.html'))
 })
+
+app.get('/utenti/:id_utente', (req, res) => {
+  console.log(`questa è la pagina dell'utente con id ${req.params.id_utente}`)
+  res.send(`questa è la pagina dell'utente con id ${req.params.id_utente}`)
+})
+
+//app.get('/articoli', (req, res) => {
+//  res.status(200).sendFile(path.join(__dirname,  '/views/articoli.html'))
+//})
+
 app.get('/articoli', (req, res) => {
-  res.status(200).sendFile(path.join(__dirname,  '/views/articoli.html'))
+  console.log(`Questa pagina contiene gli articoli dell'utente con id ${req.query.utente}`)
+  res.send(`Questa pagina contiene gli articoli dell'utente con id ${req.query.utente}`)
 })
+
 app.get('*', (req, res) => {
   res.status(200).sendFile(path.join(__dirname,  '/views/notfound.html'))
 })
 
-app.listen( 5000, () => console.log('5000'));
+app.listen( 3000, () => console.log('Listening on port 3000'));
